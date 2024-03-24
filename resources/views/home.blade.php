fa283ac8a1b33026a4f7c338155615417e25977a
Viano Di Napoli <vianodinapoli@gmail.com>
2024-03-24 20:47:25 +0200

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,14 +22,14 @@
     </div>
 
     <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
+    <div  class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-success shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                            PAIOL 4</div>
-                        <div class="h6 mb-0 font-weight-bold text-gray-600">Ver gemulex</div>
+                        <div  class="h6 mb-0 font-weight-bold text-gray-600" ><a href="{{route('gemulex.index')}}">Ver gemulex</a></div>
                     </div>
                     <div class="col-auto">
                         <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -75,8 +75,8 @@
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                            Pending Requests</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                            FÁBRICA DE ANCO</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="{{route('anfo.index')}}">Mais detalhes</a></div>
                     </div>
                     <div class="col-auto">
                         <i class="fas fa-comments fa-2x text-gray-300"></i>
